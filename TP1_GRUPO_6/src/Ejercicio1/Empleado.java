18aa8e032935f19fba1b20dd474c0e8ad72743d2
FacundoPino <facunahuelp@gmail.com>
2024-08-13 13:01:20 -0300

    Punto 3 y 4

--- TP1_GRUPO_6/src/Ejercicio1/Empleado.java
+++ TP1_GRUPO_6/src/Ejercicio1/Empleado.java
@@ -1,8 +1,39 @@
 package Ejercicio1;
 
 public class Empleado {
-	int id;
+	int final id = 1000;
 	String nombre;
 	int edad;
-
+    private static int cont=0;    
+    
+    //CONSTRUCTORES
+    public Empleado ()
+    {
+    	this.cont++;
+    	this.id = cont;
+    }
+    
+    //GETS Y SETS
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+	
+	
 }
+
